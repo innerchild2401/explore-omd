8abfa76f3abaa45251a45fcfd28f0b3c5261ea2f
afili <afilip.mme@gmail.com>
2025-10-30 17:55:02 +0200

    Clean up temporary SQL diagnostic files

--- check_grand_hotel_data.sql
+++
@@ -1,8 +0,0 @@
--- Query 1: Find the business record for Grand Hotel Caraiman
-SELECT 
-    id as business_id,
-    name as business_name,
-    slug as business_slug
-FROM businesses 
-WHERE slug = 'grand-hotel-caraiman' OR name ILIKE '%grand hotel caraiman%';
-
